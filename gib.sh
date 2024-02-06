2371752116bd895069f39c9bc714bc1fd5e4a3d3
Eason Chai <echai2905@gmail.com>
2024-02-07 00:34:32 +0800

    feat: Add gib image function with random width and height

--- gib.sh
+++ gib.sh
@@ -6,3 +6,30 @@ rand() {
     local max=$2
     echo $((RANDOM%(max-min+1)+min))
 }
+
+# Default width and height with increased range
+WIDTH=$(rand 300 1080)
+HEIGHT=$(rand 300 1080)
+
+# Function to download image
+gib_image() {
+    local filename="image-$(date +%s)"
+    if [ ! -z "$1" ]; then
+        filename=$1
+    fi
+    curl -s -L "https://picsum.photos/${WIDTH}/${HEIGHT}?random" -o "${filename}.png"
+    echo "Image saved as ${filename}.png"
+}
+
+# Main function
+case $1 in
+    meme)
+        gib_meme $2
+        ;;
+    image)
+        gib_image $2
+        ;;
+    *)
+        echo "Usage: gib [meme|image] [optional: filename]"
+        ;;
+esac
